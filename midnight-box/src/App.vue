c04fb902c6c13f0ddaeae6ded79b3e581c4db24f
Lærke Nielsen <nicolenielsen85@gmail.com>
2025-08-17 20:57:16 +0200

    Started the setup for the homepage

---
+++ midnight-box/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import HomeView from './components/HomeView.vue'
+</script>
+
+<template>
+  <header>
+    <nav>
+
+    </nav>
+  </header>
+
+  <main>
+    <HomeView />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ midnight-box/src/components/HomeView.vue
@@ -0,0 +1,176 @@
+<template>
+    <section class="hero">
+        <div class="container flex flex-col justify-center h-full">
+            <h4 class="text-white-text">A new limited edition book monthly</h4>
+            <h1 class="text-secondary leading-tight">Fantasy limited<br> book subscription</h1>
+            <p class="text-white-text pt-4 pb-6 max-w-lg">Whatever is it you like to read about, we will fulfill with each month featuring a whole new book.</p>
+            <div>
+                <button class="btn-primary">Start your subscription</button>
+            </div>
+        </div>
+        <div class="overlay"></div>
+    </section>
+
+    <section class="bg-tertiary">
+        <div class="container">
+            <h3 class="text-primary seven-percent">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque augue lorem, molestie non luctus eget, vulputate eget eros.  orci dapibus nec.</h3>
+        </div>
+        <svg class="wave wave-bottom" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#8BC9E8" d="M0,192L30,208C60,224,120,256,180,256C240,256,300,224,360,192C420,160,480,128,540,128C600,128,660,160,720,186.7C780,213,840,235,900,250.7C960,267,1020,277,1080,250.7C1140,224,1200,160,1260,154.7C1320,149,1380,203,1410,229.3L1440,256L1440,320L0,320Z"></path>
+        </svg>
+    </section>
+
+    <section class="bg-secondary">
+
+        <div class="container grid grid-cols-3 gap-6 text-center">
+            <div>
+                <div>
+                    <img src="../assets/images/adult.jpg" alt="" class="steps-img">
+                </div>
+                <div class="text-primary">
+                    <h2>Step 1.</h2>
+                    <h4 class="pb-2">Choose your plan</h4>
+                    <p class="text-black-text">Sign up and become excited to recieve your new limited edition book every month.</p>
+                </div>
+            </div>
+
+            <div>
+                <div>
+                    <img src="../assets/images/adult.jpg" alt="" class="steps-img">
+                </div>
+                <div class="text-primary">
+                    <h2>Step 2.</h2>
+                    <h4 class="pb-2">Your book arrive</h4>
+                    <p class="text-black-text">Enjoy the book shipped directly to your address.</p>
+                </div>
+            </div>
+
+            <div>
+                <div>
+                    <img src="../assets/images/adult.jpg" alt="" class="steps-img">
+                </div>
+                <div class="text-primary">
+                    <h2>Step 3.</h2>
+                    <h4 class="pb-2">Unbox the fantasy</h4>
+                    <p class="text-black-text">Sit back and enjoy your book the best way you know - wether that's having a cup of tea of soft blanket.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="bg-secondary">
+        <div class="container">
+            <div class="seven-percent max-w-4xl">
+                <h2>What makes our boxes unique?</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque augue lorem, molestie non luctus eget, vulputate eget eros. Vestibulum rutrum, purus vitae tincidunt imperdiet, nunc turpis bibendum nisl, in bibendum nibh magna at odio. Nam condimentum arcu ipsum, ac efficitur orci dapibus nec. Praesent at tincidunt nisi. Donec tortor elit, fringilla cursus leo et, bibendum vestibulum lorem. Morbi ac maximus dolor. Nulla ut elit molestie, sagittis tellus sed, dictum ante. Maecenas eget libero a nulla efficitur fringilla. Nunc vitae erat tortor. Mauris eget cursus magna, quis dignissim orci.</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="bg-primary">
+        <div class="container">
+            <div>
+                <img src="" alt="">
+                <h2>Subscription boxes</h2>
+                <img src="" alt="">
+            </div>
+    
+            <div>
+                <div>
+                    <img src="" alt="">
+                    <h4><span>€30 </span>/month</h4>
+                    <span>Adult fantasy</span>
+                    <p>For those who want a adult fantasy</p>
+                    <button class="btn-primary">Learn more</button>
+                </div>
+                <div>
+                    <img src="" alt="">
+                    <h4><span>€30 </span>/month</h4>
+                    <span>Young adult fantasy</span>
+                    <p>For those who want a young adult fantasy</p>
+                    <button class="btn-primary">Learn more</button>
+                </div>
+                <div>
+                    <img src="" alt="">
+                    <h4><span>€30 </span>/month</h4>
+                    <span>Mix of everything fantasy</span>
+                    <p>If you like everything</p>
+                    <button class="btn-primary">Learn more</button>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="bg-primary">
+        <div class="container">
+            <h2>What makes our boxes unique?</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque augue lorem, molestie non luctus eget, vulputate eget eros. Vestibulum rutrum, purus vitae tincidunt imperdiet, nunc turpis bibendum nisl, in bibendum nibh magna at odio. Nam condimentum arcu ipsum, ac efficitur orci dapibus nec. Praesent at tincidunt nisi. Donec tortor elit, fringilla cursus leo et, bibendum vestibulum lorem. Morbi ac maximus dolor. Nulla ut elit molestie, sagittis tellus sed, dictum ante. Maecenas eget libero a nulla efficitur fringilla. Nunc vitae erat tortor. Mauris eget cursus magna, quis dignissim orci.</p>
+        </div>
+    </section>
+
+    <section class="bg-tertiary">
+        <div class="container">
+            <div>
+                <span>Take a look at</span>
+                <h4>Previous boxes</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque augue lorem, molestie non luctus eget, vulputate eget eros. Vestibulum rutrum, purus vitae tincidunt imperdiet, nunc turpis bibendum nisl, in bibendum nibh magdictum ante.</p>
+            </div>
+    
+            <div>
+                swiper stuff
+            </div>
+        </div>
+    </section>
+
+    <section class="bg-tertiary">
+        <div class="container">
+            <img src="" alt="">
+            <h2>What our customers say</h2>
+            <p>Jeg har lige modtaget min pakke og de er smpelhen bare så flotte de bøger herJeg har lige modtaget min pakke og de er smpelhen bare så flotte de bøger herJeg har lige modtaget min pakke og de er smpelhen bare så flotte de bøger herJeg har lige modtaget min pakke og de er smpelhen bare så flotte de bøger herJeg har lige modtaget min pakke og de er smpelhen bare så flotte de bøger herJeg har lige modtaget min pakke og de er smpelhen bare så flotte de bøger herJeg har lige modtaget min pakke og de er smpelhen bare så flotte de bøger her</p>
+            <p>Jane doe, <span class="font-bold">Young adult</span></p>
+            <img src="" alt="">
+        </div>
+    </section>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.hero {
+  position: relative; 
+  background-image: url('../assets/images/hero.jpeg');
+  background-size: cover;
+  background-position: center;
+  height: 90vh;
+}
+
+.hero::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(37, 28, 75, 0.5);
+  z-index: 0;
+}
+
+.hero > * {
+  position: relative;
+  z-index: 1;
+}
+
+
+
+/* steps */
+.steps-img {
+    width: 100%;
+    height: 400px;
+    object-fit: cover;
+    border-radius: 100% 100% 15px 15px;
+    margin-bottom: 8px;
+}
+
+</style>
